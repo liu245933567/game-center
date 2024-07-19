68c642e1c87e294246bdff1600363b3e6145449c
Lenin <41979955+liu245933567@users.noreply.github.com>
2024-07-19 20:15:40 +0800

    ✨ feat: 添加布局组件

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { NextUIProvider } from "@nextui-org/react";
+import { Layout } from "@/components";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="zh-CN">
       <body className={inter.className}>
-        <NextUIProvider>{children}</NextUIProvider>
+        <NextUIProvider>
+          <Layout>{children}</Layout>
+        </NextUIProvider>
       </body>
     </html>
   );
